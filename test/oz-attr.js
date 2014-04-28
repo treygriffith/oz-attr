2e9387e23eda09dec3bc659b99d0adaddec7da87
Trey Griffith <trey.griffith@gmail.com>
2014-04-28 14:14:27 -0700

    add tests

---
+++ test/oz-attr.js
@@ -0,0 +1,34 @@
+
+var Oz = require('oz');
+var assert = require('assert');
+var children = require('children');
+
+describe('Rendering', function(){
+
+  it('should set attributes without changing context', function(){
+    var el = children(Oz.render('<div oz-attr="class:name"><p oz-text="text"></p></div>', { name: 'Tobi', text: 'something'}));
+    assert('Tobi' == el[0].className);
+  });
+
+  it('should render multiple attributes in one tag', function(){
+    var el = children(Oz.render('<div oz-attr="class:name;data-active:active"></div>', { name: 'Tobi', active: true})); 
+    assert('Tobi' == el[0].className);
+    assert('true' == el[0].getAttribute('data-active'));
+  });
+
+});
+
+describe("Updating", function() {
+
+  it('should update multiple attributes in one tag', function(){
+    var template = Oz('<div oz-attr="class:name;data-active:active"></div>');
+    var el = children(template.render({ name: 'Tobi', active: true}));
+    assert('Tobi' == el[0].className);
+    assert('true' == el[0].getAttribute('data-active'));
+
+    template.update({ name: 'Paul', active: false});
+    assert('Paul' == el[0].className);
+    assert('false' == el[0].getAttribute('data-active'));
+  });
+
+});
